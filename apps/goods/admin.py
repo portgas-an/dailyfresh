2eecbbcaa39ea1388346ec8e8e582f12f814b6f9
ace <1@1.com>
2019-05-19 16:04:27 +0800

    修改后台数据生成静态页面

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from goods.models import GoodsType, IndexPromotionBanner
 # Register your models here.
 
+
+class IndexPromotionBannerAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        """更新表中的数据时"""
+        super().save_model(request, obj, form, change)
+
+        # 发出任务，celery_worker重新生成静态页面
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+    def delete_model(self, request, obj):
+        super().delete_model(request, obj)
+
+        # 发出任务，celery_worker重新生成静态页面
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+
 admin.site.register(GoodsType)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -1,14 +1,17 @@
 import django
 import time
 from celery import Celery
-from django.conf import settings
-from django.core.mail import send_mail
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailyfresh.settings')
 django.setup()
 
 app = Celery('celery_tasks.tasks', broker='redis://127.0.0.1:6379/8')
+
+from goods.models import *
+from django.template import loader
+from django.conf import settings
+from django.core.mail import send_mail
 
 
 @app.task
@@ -24,5 +27,39 @@ def send_register_active_email(to_email, username, token):
     time.sleep(5)
 
 
+@app.task
 def generate_static_index_html():
     """产生静态页面"""
+    # 获取商品的种类信息
+    types = GoodsType.objects.all()
+
+    # 获取首页轮播商品信息
+    goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    # 获取首页促销活动信息
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    # 获取首页分类商品展示信息
+    for type in types:  # GoodsType
+        # 获取type种类首页分类商品的图片展示信息
+        image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+        # 获取type种类首页分类商品的文字展示信息
+        title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+
+        # 动态给type增加属性，分别保存首页分类商品的图片展示信息和文字展示信息
+        type.image_banners = image_banners
+        type.title_banners = title_banners
+
+    context = {'types': types,
+               'goods_banners': goods_banners,
+               'promotion_banners': promotion_banners}
+
+    # 加载模板
+    temp = loader.get_template('static_index.html')
+    # 渲染模板
+    static_index_html = temp.render(context)
+    # 生成首页对应的静态文件
+    save_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+
+    with open(save_path, 'w') as f:
+        f.write(static_index_html)
